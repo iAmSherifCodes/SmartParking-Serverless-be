294c852d5d0e2af7418863959e5a366214162774
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-10-23 11:34:38 +0100

    Initial commit

---
+++ test/parking_serverless.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ParkingServerless = require('../lib/parking_serverless-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/parking_serverless-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ParkingServerless.ParkingServerlessStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
