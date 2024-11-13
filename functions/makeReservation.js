6d24500cf828240eaaab15a93552bcf8120bb3ed
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-11-13 17:53:57 +0100

    Implemented crypto for id generator

--- functions/makeReservation.js
+++ functions/makeReservation.js
@@ -1,72 +1,87 @@
-const { parkingReservationSchema } = require("../utils/schemaValidation");
 const {
   DynamoDBDocumentClient,
   UpdateCommand,
+  GetCommand,
   PutCommand,
 } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
-const getSpaceBySpaceNumber = require("../repositories/getSpaceBySpaceNumber");
+// const getSpaceBySpaceNumber = require("../repositories/getSpaceBySpaceNumber");
 const dynamodbClient = new DynamoDB();
 const dynamodb = DynamoDBDocumentClient.from(dynamodbClient);
 const parkingSpaceTable = process.env.PARKING_SPACE_TABLE;
 const reservationTable = process.env.RESERVATION_TABLE;
-const uuid = require("uuid");
+const crypto = require('crypto')
 
-const reserveParkingSpace = async (req) => {
+
+const generateId = () => {
+  const length = 7;
+  return crypto
+    .randomBytes(length)
+    .toString("base64")
+    .replace(/\//g, "_")
+    .replace(/\+/g, "-")
+    .slice(0, length);
+};
+
+const saveReservation = async (spaceNumber, reserveTime) => {
   try {
+    const v4 = generateId();
+    const params = {
+      TableName: reservationTable,
+      Item: {
+        reservation_id: v4,
+        space_no: spaceNumber,
+        // user_id: event.userId,
+        reserve_time: reserveTime,
+        // start_time: startTime,
+        // end_time: event.endTime,
+      },
+    };
+    const putCommand = new PutCommand(params);
+    const res = await dynamodb.send(putCommand);
+    console.log("SAVED DATA", res)
+    // return ({
+    //   data: res
+    // })
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Invalid request body",
+        error: error.message || error,
+      }),
+    };
+  }
+};
 
-    // reserve time
-    // check in time
-    // space number
-    const value = await parkingReservationSchema.validateAsync(req);
 
-    const parsedStartTime = new Date(value.startTime);
-    const parsedReservedTime = new Date(value.reserveTime);
-    const currentDate = new Date();
-
-    if (parsedStartTime < currentDate) {
-      return {
-        status: 400,
-        success: false,
-        message: "Start time cannot be in the past.",
-      };
-    }
-
-    // Validation: End time must be within 2 days of start time
-    const maxEndTime = new Date(parsedStartTime);
-    maxEndTime.setDate(maxEndTime.getDate() + 2);
-
-    if (parsedReservedTime > maxEndTime) {
-      return {
-        status: 400,
-        success: false,
-        message: "Check-in time cannot be more than 2 days from the start time.",
-      };
-    }
-
+const reserveParkingSpace = async (spaceNumber, reserveTime) => {
+  try {
     // Check if the parking space is available in parking space table
     const spaceAvailability = await getSpaceBySpaceNumber(
-      value.spaceNumber,
+      spaceNumber,
       parkingSpaceTable
     );
     if (!spaceAvailability.success) {
       return {
-        status: 404,
-        success: false,
-        message: "Parking space is already reserved",
+        statusCode: 404,
+        body: "Parking space is already reserved",
       };
     }
     // write the space number to its table (parking space table) as reserved
-    await updateReserveTable(value.spaceNumber);
+    await updateReserveTable(spaceNumber);
     // write to reservation table (user details, reserve time)
-    const savedData = await saveReservation(value.spaceNumber, value.reserveTime, value.startTime);
+    await saveReservation(
+      spaceNumber,
+      reserveTime
+    );
     // return reservationId, spaceNumber, userId with successfull message sent to their email
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: "Parking space reserved successfully",
         data: value,
-        value: savedData
       }),
     };
   } catch (error) {
@@ -87,9 +102,10 @@ const updateReserveTable = async (spaceNumber) => {
       Key: {
         space_no: spaceNumber,
       },
-      UpdateExpression: "SET is_reserved = :reserved",
+      UpdateExpression: "SET is_reserved = :reserved, status = :available",
       ExpressionAttributeValues: {
         ":reserved": true,
+        ":available": "reserved"
       },
       ReturnValues: "ALL_NEW",
     };
@@ -108,34 +124,73 @@ const updateReserveTable = async (spaceNumber) => {
   }
 };
 
-const saveReservation = async (spaceNumber, reserveTime, startTime) => {
-  try {
-    const params = {
-      TableName: reservationTable,
-      Item: {
-        reservation_id: uuid.v6,
-        space_no: spaceNumber,
-        // user_id: event.userId,
-        reserve_time: reserveTime,
-        start_time: startTime,
-        // end_time: event.endTime,
+const getSpaceBySpaceNumber = async (spaceNumber, tableName) => {
+  const space = new GetCommand({
+      TableName: tableName,
+      Key: {
+          space_no: spaceNumber,
       },
-    };
-    const putCommand = new PutCommand(params);
-    const res = await dynamodb.send(putCommand);
+      ConsistentRead: true,
+      ReturnConsumedCapacity: "NONE",
+  })
+  console.log("Space availability", space)
+  try {
+      const result = await dynamodb.send(space);
+      return {
+        statusCode: 200,
+          message: "Parking space is available.",
+          body: result.Item
+      };
   } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: "Invalid request body",
-        error: error.message || error,
-      }),
-    };
+      console.error("Error fetching space:", error);
+      return {
+          statusCode: 400,
+          body: JSON.stringify({ message: "Invalid request body", error: error.message || error }),
+      };
   }
-};
+}
+
+
 
 module.exports.handler = async (event, context) => {
   console.log("CONTEXGT OBJ: ", context);
-  await reserveParkingSpace(event);
-  };
+  // reserve time
+  // check in time
+  // space number
+  // const value = await parkingReservationSchema.validateAsync(event.body);
+  const value = JSON.parse(event.body);
+  console.log("VALUE: ", value);
+  // const parsedStartTime = new Date(value.startTime);
+  const parsedReservedTime = new Date(value.reserveTime);
+  const currentDate = new Date();
+
+  if (parsedReservedTime < currentDate) {
+    return {
+      status: 400,
+      success: false,
+      message: "Start time cannot be in the past.",
+    };
+  }
+
+  // Validation: End time must be within 2 days of start time
+  const maxEndTime = new Date();
+  maxEndTime.setDate(maxEndTime.getDate() + 2);
+
+  if (parsedReservedTime > maxEndTime) {
+    return {
+      status: 400,
+      success: false,
+      message: "Check-in time cannot be more than 2 days from the start time.",
+    };
+  }
+
+  const data = await reserveParkingSpace(value.spaceNumber, value.reserveTime);
+  return({
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Parking space reserved successfully",
+      data: data.body.message,
+    }),
+  })
+};
+
