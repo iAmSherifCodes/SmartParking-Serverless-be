e357227c3af26914b4570497937b55020fd86fdb
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2024-11-14 02:14:12 +0100

    Configured CheckOut function as a lambda function and integrated with API gateway

--- cdk/constructs/apiStack.js
+++ cdk/constructs/apiStack.js
@@ -1,61 +1,80 @@
-const {CfnOutput, Stack, Duration} = require("aws-cdk-lib");
-const {Runtime, Function, Code} = require("aws-cdk-lib/aws-lambda");
-const {RestApi, LambdaIntegration} = require('aws-cdk-lib/aws-apigateway');
+const { CfnOutput, Stack, Duration } = require("aws-cdk-lib");
+const { Runtime, Function, Code } = require("aws-cdk-lib/aws-lambda");
+const { RestApi, LambdaIntegration } = require("aws-cdk-lib/aws-apigateway");
 
-class ApiStack extends Stack{
+class ApiStack extends Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
 
-    constructor(scope, id, props) {
-        super(scope, id, props);
+    const restApi = new RestApi(this, `${props.stageName}-MyApi`, {
+      deployOptions: {
+        stageName: props.stageName,
+      },
+    });
 
+    const parkingSpaceTable = props.parkingSpaceTable;
+    const reservationTable = props.reservationTable;
 
-        const restApi = new RestApi(this, `${props.stageName}-MyApi`, {
-            deployOptions: {
-                stageName: props.stageName
-            }
-        });
+    const viewAvailableSpots = new Function(this, "ViewAvailableSpots", {
+      runtime: Runtime.NODEJS_20_X,
+      handler: "viewAvailableSpots.handler",
+      code: Code.fromAsset("functions"),
+      timeout: Duration.seconds(30),
+      environment: {
+        TABLE_NAME: parkingSpaceTable.tableName,
+      },
+    });
 
-        const parkingSpaceTable = props.parkingSpaceTable;
-        const reservationTable = props.reservationTable;
+    parkingSpaceTable.grantReadData(viewAvailableSpots);
 
-        const viewAvailableSpots = new Function(this, "ViewAvailableSpots", {
-            runtime: Runtime.NODEJS_20_X,
-            handler: "viewAvailableSpots.handler",
-            code: Code.fromAsset("functions"),
-            timeout: Duration.seconds(30),
-            environment: {
-                TABLE_NAME: parkingSpaceTable.tableName
-            }
-        })
+    const makeReservation = new Function(this, "MakeReservation", {
+      runtime: Runtime.NODEJS_20_X,
+      handler: "makeReservation.handler",
+      timeout: Duration.seconds(30),
+      code: Code.fromAsset("functions"),
+      environment: {
+        PARKING_SPACE_TABLE: parkingSpaceTable.tableName,
+        RESERVATION_TABLE: reservationTable.tableName,
+      },
+    });
 
-        parkingSpaceTable.grantReadData(viewAvailableSpots);
+    parkingSpaceTable.grantReadWriteData(makeReservation);
+    reservationTable.grantReadWriteData(makeReservation);
 
-        const makeReservation = new Function(this, "MakeReservation",{
-            runtime: Runtime.NODEJS_20_X,
-            handler: "makeReservation.handler",
-            timeout: Duration.seconds(30),
-            code: Code.fromAsset("functions"),
-            environment: {
-                PARKING_SPACE_TABLE: parkingSpaceTable.tableName,
-                RESERVATION_TABLE: reservationTable.tableName
-            }
-        })
+    const checkOutFunction = new Function(this, "CheckOut", {
+      runtime: Runtime.NODEJS_20_X,
+      handler: "checkOut.handler",
+      timeout: Duration.seconds(30),
+      code: Code.fromAsset("functions"),
+      environment: {
+        RESERVATION_TABLE: reservationTable.tableName,
+      },
+    });
+    reservationTable.grantReadWriteData(checkOutFunction);
 
-        parkingSpaceTable.grantReadWriteData(makeReservation);
-        reservationTable.grantReadWriteData(makeReservation);
+    const viewAvailableSpotsLambdaIntegration = new LambdaIntegration(
+      viewAvailableSpots
+    );
+    const makeReservationLambdaIntegration = new LambdaIntegration(
+      makeReservation
+    );
+    const checkOutLambdaIntegration = new LambdaIntegration(checkOutFunction);
 
-        const viewAvailableSpotsLambdaIntegration = new LambdaIntegration(viewAvailableSpots);
-        const makeReservationLambdaIntegration = new LambdaIntegration(makeReservation);
+    // const rootApi = restApi.root.addResource('v2');
+    restApi.root
+      .addResource("available-spaces")
+      .addMethod("GET", viewAvailableSpotsLambdaIntegration);
+    restApi.root
+      .addResource("reserve")
+      .addMethod("POST", makeReservationLambdaIntegration);
+    restApi.root
+      .addResource("checkout")
+      .addMethod("POST", checkOutLambdaIntegration);
 
-        // const rootApi = restApi.root.addResource('v2');
-        restApi.root.addResource("available-spaces").addMethod("GET",viewAvailableSpotsLambdaIntegration);
-        restApi.root.addResource('reserve').addMethod("POST", makeReservationLambdaIntegration)
-
-        new CfnOutput(this, 'url', {
-            value: restApi.url
-        })
-    }
-
-
+    new CfnOutput(this, "url", {
+      value: restApi.url,
+    });
+  }
 }
 
-module.exports = {ApiStack}
+module.exports = { ApiStack };
