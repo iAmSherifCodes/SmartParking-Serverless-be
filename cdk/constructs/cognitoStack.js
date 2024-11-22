3686e91542eb0f6616021e459acf6bcb869d2642
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2024-11-22 13:15:23 +0100

    configured Cognito construct

---
+++ cdk/constructs/cognitoStack.js
@@ -0,0 +1,43 @@
+const { UserPool, UserPoolClient } = require("aws-cdk-lib/aws-cognito");
+const { Stack } = require("aws-cdk-lib");
+
+class CognitoStack extends Stack {
+  /**
+   * Creates a Cognito User Pool and User Pool Client
+   * @param {Construct} scope
+   * @param {string} id
+   * @param {StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const userPool = new UserPool(this, "UserPool", {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      signInCaseSensitive: false,
+      passwordPolicy: {
+        minLength: 5,
+      },
+      autoVerify: {
+        email: true,
+      },
+    });
+
+    const webUserPoolClient = new UserPoolClient(this, "WebUserPoolClient", {
+      userPool,
+      preventUserExistenceErrors: true,
+      authFlows: {
+        userSrp: true,
+      },
+    });
+
+    this.userPool = userPool;
+    this.webUserPoolClient = webUserPoolClient;
+  }
+}
+
+module.exports = {
+  CognitoStack,
+};
