eaa46803f41e35cbb08b57bd0f2ac3d6134c2ce5
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2024-11-26 00:19:42 +0100

    Commented cognito

--- cdk/constructs/cognitoStack.js
+++ cdk/constructs/cognitoStack.js
@@ -1,43 +1,43 @@
-const { UserPool, UserPoolClient } = require("aws-cdk-lib/aws-cognito");
-const { Stack } = require("aws-cdk-lib");
+// const { UserPool, UserPoolClient } = require("aws-cdk-lib/aws-cognito");
+// const { Stack } = require("aws-cdk-lib");
 
-class CognitoStack extends Stack {
-  /**
-   * Creates a Cognito User Pool and User Pool Client
-   * @param {Construct} scope
-   * @param {string} id
-   * @param {StackProps=} props
-   */
-  constructor(scope, id, props) {
-    super(scope, id, props);
+// class CognitoStack extends Stack {
+//   /**
+//    * Creates a Cognito User Pool and User Pool Client
+//    * @param {Construct} scope
+//    * @param {string} id
+//    * @param {StackProps=} props
+//    */
+//   constructor(scope, id, props) {
+//     super(scope, id, props);
 
-    const userPool = new UserPool(this, "UserPool", {
-      selfSignUpEnabled: true,
-      signInAliases: {
-        email: true,
-      },
-      signInCaseSensitive: false,
-      passwordPolicy: {
-        minLength: 5,
-      },
-      autoVerify: {
-        email: true,
-      },
-    });
+//     const userPool = new UserPool(this, "UserPool", {
+//       selfSignUpEnabled: true,
+//       signInAliases: {
+//         email: true,
+//       },
+//       signInCaseSensitive: false,
+//       passwordPolicy: {
+//         minLength: 5,
+//       },
+//       autoVerify: {
+//         email: true,
+//       },
+//     });
 
-    const webUserPoolClient = new UserPoolClient(this, "WebUserPoolClient", {
-      userPool,
-      preventUserExistenceErrors: true,
-      authFlows: {
-        userSrp: true,
-      },
-    });
+//     const webUserPoolClient = new UserPoolClient(this, "WebUserPoolClient", {
+//       userPool,
+//       preventUserExistenceErrors: true,
+//       authFlows: {
+//         userSrp: true,
+//       },
+//     });
 
-    this.userPool = userPool;
-    this.webUserPoolClient = webUserPoolClient;
-  }
-}
+//     this.userPool = userPool;
+//     this.webUserPoolClient = webUserPoolClient;
+//   }
+// }
 
-module.exports = {
-  CognitoStack,
-};
+// module.exports = {
+//   CognitoStack,
+// };
