4a132e2a9a4b96722490b38bfcdf83cef25c7ff1
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-03 23:56:45 +0100

    Fixed CORS and optimized payment calls

--- functions/initiatePayment.js
+++ functions/initiatePayment.js
@@ -1,12 +1,16 @@
 const axios = require('axios');
 const {
     DynamoDBDocumentClient,
-    GetCommand
-  } = require("@aws-sdk/lib-dynamodb");
-  const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+    GetCommand,
+    UpdateCommand
+} = require("@aws-sdk/lib-dynamodb");
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const dynamodb = DynamoDBDocumentClient.from(new DynamoDB());
 const { PAYMENT_HISTORY_TABLE: paymentHistoryTable, FLW_SECRET_KEY } = process.env;
 
+
+const ALLOWED_ORIGINS = ['http://localhost:3002'];
+const FLUTTERWAVE_API = 'https://api.flutterwave.com/v3/payments';
 const fetchPaymentDetails = async (paymentId) => {
     const params = {
         TableName: paymentHistoryTable,
@@ -17,21 +21,27 @@ const fetchPaymentDetails = async (paymentId) => {
     return Item;
 };
 
-// const updatePaymentStatus = async (paymentId, status) => {
-//     const params = {
-//         TableName: paymentHistoryTable,
-//         Key: { id: paymentId },
-//         UpdateExpression: "SET #paymentStatus = :status",
-//         ExpressionAttributeNames: {
-//             "#paymentStatus": "paymentStatus",
-//         },
-//         ExpressionAttributeValues: {
-//             ":status": status,
-//         },
-//     };
+const updatePaymentStatus = async (paymentId, status) => {
+    try {
+        const params = {
+            TableName: paymentHistoryTable,
+            Key: { id: paymentId },
+            UpdateExpression: "SET #paymentStatus = :status",
+            ExpressionAttributeNames: {
+                "#paymentStatus": "paymentStatus",
+            },
+            ExpressionAttributeValues: {
+                ":status": status,
+            },
+        };
 
-//     await dynamodb.send(new UpdateCommand(params));
-// };
+        await dynamodb.send(new UpdateCommand(params));
+    } catch (error) {
+        console.error('Error updating payment status:', error);
+        throw error;
+    }
+
+};
 
 module.exports.handler = async (event, context) => {
 
@@ -39,32 +49,42 @@ module.exports.handler = async (event, context) => {
         const body = parseEventBody(event.body);
         const paymentDetails = await fetchPaymentDetails(body.paymentId);
 
+        if (paymentDetails.paymentStatus === 'successful'){
+            return createResponse(400, { error: 'Payment already successful' });
+        }
+
         const paymentData = {
             currency: "NGN",
             amount: paymentDetails.charge,
-            customer:{
+            customer: {
                 email: paymentDetails.userDetails,
             },
             customizations: {
                 title: 'Smart Parking Payment',
                 description: "Please proceed to checkout"
-              },
+            },
             tx_ref: context.awsRequestId.toString(),
             redirect_url: "www.google.com",
         };
 
-        const response =  await axios.post(
-            'https://api.flutterwave.com/v3/payments', paymentData, {
-                headers: {
-                  Authorization: `Bearer ${FLW_SECRET_KEY}`,
-                  'Content-Type': 'application/json'
-                }
-              });
+        const response = await axios.post(
+            FLUTTERWAVE_API, paymentData, {
+            headers: {
+                Authorization: `Bearer ${FLW_SECRET_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
 
         if (response.data.status === 'success') {
             console.log('Card Charge Successful', response.data);
-            // await updatePaymentStatus(body.paymentId, 'successful');
-            return createResponse(200, response.data);
+            await updatePaymentStatus(body.paymentId, 'successful');
+            return createResponse(302, response.data, {
+                'Access-Control-Allow-Origin': ALLOWED_ORIGINS[0],
+                'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Api-Key,X-Amz-Date,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                Location: response.data.data.link
+            });
         } else {
             console.log('Card Charge Failed', response);
             throw new Error(response);
@@ -79,11 +99,21 @@ function parseEventBody(body) {
     return typeof body === "string" ? JSON.parse(body) : body;
 }
 
-function createResponse(statusCode, body) {
-    return {
-        statusCode,
-        body: JSON.stringify(body)
+function createResponse(statusCode, body, headers) {
+    if (!headers) {
+        headers = {
+            'Access-Control-Allow-Origin': ALLOWED_ORIGINS[0],
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Api-Key,X-Amz-Date,X-Amz-Security-Token',
+            'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
+        }
     };
+
+return {
+    statusCode,
+    body: JSON.stringify(body),
+    headers
+};
 }
 
 
