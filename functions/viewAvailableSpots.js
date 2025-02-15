cc6e2aa26d2fc23744dd5b5b3db60823399a8501
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-15 16:25:08 +0100

    Updated CORS

--- functions/viewAvailableSpots.js
+++ functions/viewAvailableSpots.js
@@ -39,8 +39,10 @@ const createResponse = (statusCode, body) => ({
     statusCode,
     body: JSON.stringify(body),
     headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:3002',
+        'Access-Control-Allow-Credentials': true,
         'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Api-Key,X-Amz-Date,X-Amz-Security-Token',
-        'Access-Control-Allow-Methods': 'GET'
+        'Access-Control-Allow-Methods': 'POST,OPTIONS'
     }
 });
 
