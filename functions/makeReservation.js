5fb7c40f9d5afedd25a504bc78fc3a942b9628e5
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-17 19:03:51 +0100

    Restructuring business model

--- functions/makeReservation.js
+++ functions/makeReservation.js
@@ -1,55 +1,89 @@
 const {
   DynamoDBDocumentClient,
-  UpdateCommand,
+  // UpdateCommand,
   GetCommand,
   PutCommand,
 } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
-const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
-const sesClient = new SESClient({ region: "us-east-1" });
+// const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+// const sesClient = new SESClient({ region: "us-east-1" });
 const moment = require("moment-timezone");
 
 const dynamodb = DynamoDBDocumentClient.from(new DynamoDB());
-const { PARKING_SPACE_TABLE: parkingSpaceTable, RESERVATION_TABLE: reservationTable, VERIFIED_EMAIL: verifiedEmail, COMPANY_NAME:companyName } = process.env;
+const {
+  PARKING_SPACE_TABLE: parkingSpaceTable,
+  PAYMENT_HISTORY_TABLE: paymentHistoryTable,
+  // RESERVATION_TABLE: reservationTable,
+  // VERIFIED_EMAIL: verifiedEmail,
+  // COMPANY_NAME:companyName
+} = process.env;
 
 
 const TIMEZONE = "Africa/Lagos";
-const STATUS = {
-  AVAILABLE: "available",
-  RESERVED: "reserved"
-};
+const RATE_PER_10_MINS = 105.99;
+const TEN_MINUTES_IN_MS = 10 * 60 * 1000;
 
 
 class ParkingService {
-  static async saveReservation(spaceNumber, reserveTime, id, email) {
+  // static async saveReservation(spaceNumber, reserveTime, id, email) {
+  //   const params = {
+  //     TableName: reservationTable,
+  //     Item: { id, space_no: spaceNumber, reserve_time: reserveTime, userEmail: email }
+  //   };
+
+  //   await dynamodb.send(new PutCommand(params));
+  //   return { id, spaceNumber, reserveTime, email };
+  // }
+
+  static async saveBill(spaceNumber, currentTime, checkoutTime, charge, id, email) {
     const params = {
-      TableName: reservationTable,
-      Item: { id, space_no: spaceNumber, reserve_time: reserveTime, userEmail: email }
+      TableName: paymentHistoryTable,
+      Item: {
+        id,
+        userEmail: email,
+        space_no: spaceNumber,
+        reserve_time: currentTime,
+        charge,
+        checkout_time: checkoutTime,
+        paymentStatus: "unprocessed",
+      },
     };
 
     await dynamodb.send(new PutCommand(params));
-    return { id, spaceNumber, reserveTime, email };
+    return { id };
   }
 
-  static async updateReserveTable(spaceNumber, date) {
-    const params = {
-      TableName: parkingSpaceTable,
-      Key: { space_no: spaceNumber },
-      UpdateExpression: "SET #is_reserved = :reserved, #status = :status, #reserved_date = :date",
-      ExpressionAttributeNames: {
-        "#status": "status",
-        "#is_reserved": "reserved",
-        "#reserved_date": "date",
-      },
-      ExpressionAttributeValues: {
-        ":reserved": true,
-        ":status": STATUS.RESERVED,
-        ":date": date
-      },
-      ReturnValues: "ALL_NEW"
-    };
+  // static async updateReserveTable(spaceNumber, date) {
+  //   const params = {
+  //     TableName: parkingSpaceTable,
+  //     Key: { space_no: spaceNumber },
+  //     UpdateExpression: "SET #is_reserved = :reserved, #reserved_date = :date",
+  //     ExpressionAttributeNames: {
+  //       "#is_reserved": "reserved",
+  //       "#reserved_date": "date",
+  //     },
+  //     ExpressionAttributeValues: {
+  //       ":reserved": true,
+  //       ":date": date
+  //     },
+  //     ReturnValues: "ALL_NEW"
+  //   };
 
-    return dynamodb.send(new UpdateCommand(params));
+  //   return dynamodb.send(new UpdateCommand(params));
+  // }
+
+  static async calculateCharge(currentTime, checkoutTime) {
+    const reservationDateTime = new Date(currentTime);
+    const checkoutDateTime = new Date(checkoutTime);
+    const timeDifference = checkoutDateTime - reservationDateTime;
+    if (timeDifference < 0) {
+      return RATE_PER_10_MINS;
+    } else if (timeDifference < TEN_MINUTES_IN_MS) {
+      return RATE_PER_10_MINS;
+    } else {
+      const numberOf30Mins = Math.round(timeDifference / TEN_MINUTES_IN_MS);
+      return numberOf30Mins === 0 ? RATE_PER_10_MINS : numberOf30Mins * RATE_PER_10_MINS;
+    }
   }
 
   static async getSpaceBySpaceNumber(spaceNumber) {
@@ -75,20 +109,24 @@ const createResponse = (statusCode, body) => ({
   }
 });
 
-const validateReservationTime = (reservationTime, currentTime) => {
-  if (reservationTime < currentTime) {
+const validateReservationTime = (checkoutTime, currentTime) => {
+  if (checkoutTime < currentTime) {
     throw new Error("Start time cannot be in the past.");
+  }
+
+  if (checkoutTime.diff(currentTime, 'hours') > 24) {
+    throw new Error("Reservation time cannot exceed 24 hours from now.");
   }
 };
 
 module.exports.handler = async (event, context) => {
   try {
 
-    const { reserveTime, spaceNumber, email } = JSON.parse(
+    const { checkoutTime, spaceNumber, email } = JSON.parse(
       typeof event.body === "string" ? event.body : JSON.stringify(event.body)
     );
 
-    if(!email){
+    if (!email) {
       return createResponse(400, {
         success: false,
         message: "Email is required"
@@ -96,25 +134,25 @@ module.exports.handler = async (event, context) => {
     }
 
     const currentTime = moment().tz(TIMEZONE);
-    const reservationTime = moment(reserveTime).tz(TIMEZONE);
-    validateReservationTime(reservationTime, currentTime);
+    const formattedCheckoutTime = moment(checkoutTime).tz(TIMEZONE);
+    validateReservationTime(formattedCheckoutTime, currentTime);
 
     // Check space availability
     const space = await ParkingService.getSpaceBySpaceNumber(spaceNumber);
-    if (!space || space.status !== STATUS.AVAILABLE) {
+    if (!space || space.reserved === true) {
       return createResponse(404, {
         success: false,
         message: `Parking space ${spaceNumber} is not available`
       });
     }
 
-    await ParkingService.updateReserveTable(spaceNumber, reserveTime);
-    const reservation = await ParkingService.saveReservation(
-      spaceNumber,
-      reservationTime.format(),
-      context.awsRequestId.toString(),
-      email
-    );
+    // await ParkingService.updateReserveTable(spaceNumber, reserveTime);
+    // const reservation = await ParkingService.saveReservation(
+    //   spaceNumber,
+    //   formattedCheckoutTime.format(),
+    //   context.awsRequestId.toString(),
+    //   email
+    // );
 
     // send email to user
     // const emailParams = {
@@ -135,10 +173,23 @@ module.exports.handler = async (event, context) => {
     // };
     // await sesClient.send(new SendEmailCommand(emailParams));
 
+
+    const charge = await ParkingService.calculateCharge(currentTime, formattedCheckoutTime);
+    const { id } = await ParkingService.saveBill(
+      spaceNumber,
+      currentTime.format(),
+      formattedCheckoutTime.format(),
+      charge,
+      context.awsRequestId.toString(),
+      email
+    );
     return createResponse(200, {
       success: true,
-      message: "Parking space reserved successfully",
-      data: reservation
+      message: "Proceed to payment",
+      data: {
+        charge,
+        paymentId: id
+      }
     });
 
   } catch (error) {
