9bd1216132b89322d1e6a79d8c2b359ba56c9a8f
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-07-16 00:59:32 +0100

    Add/update file: repositories/getSpaceBySpaceNumber.js

--- repositories/getSpaceBySpaceNumber.js
+++ repositories/getSpaceBySpaceNumber.js
@@ -1,33 +1,60 @@
-const {DynamoDBDocumentClient, GetCommand} = require("@aws-sdk/lib-dynamodb");
-const {DynamoDB} = require("@aws-sdk/client-dynamodb");
-const dynamodbClient = new DynamoDB();
-const dynamodb = DynamoDBDocumentClient.from(dynamodbClient);
+const { GetCommand } = require("@aws-sdk/lib-dynamodb");
+const databaseClient = require('../utils/database');
+const logger = require('../utils/logger');
+const { NotFoundError } = require('../utils/errors');
 
-const getSpaceBySpaceNumber = async (spaceNumber, tableName) => {
-    const space = new GetCommand({
-        TableName: tableName,
-        Key: {
-            space_no: spaceNumber,
-        },
-        ConsistentRead: true,
-        ReturnConsumedCapacity: "NONE",
-    })
-    console.log("Space availability", space)
-    try {
-        const result = await dynamodb.send(space);
-        return {
-            status: 200,
-            success: true,
-            message: "Parking space is available.",
-            data: result.Item
-        };
-    } catch (error) {
-        console.error("Error fetching space:", error);
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: "Invalid request body", error: error.message || error }),
-        };
+class ParkingSpaceRepository {
+  constructor(tableName) {
+    this.tableName = tableName;
+    this.docClient = databaseClient.getDocumentClient();
+  }
+
+  async getBySpaceNumber(spaceNumber) {
+    const command = new GetCommand({
+      TableName: this.tableName,
+      Key: { space_no: spaceNumber },
+      ConsistentRead: true,
+    });
+
+    logger.debug('Fetching parking space', { spaceNumber, tableName: this.tableName });
+    
+    const result = await databaseClient.executeCommand(command, 'get parking space');
+    
+    if (!result.Item) {
+      throw new NotFoundError(`Parking space ${spaceNumber}`);
     }
+
+    return result.Item;
+  }
+
+  async updateReservationStatus(spaceNumber, reserved, reservationDate = null) {
+    const { UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+    
+    const updateExpression = reserved 
+      ? "SET reserved = :reserved, reservation_date = :date"
+      : "SET reserved = :reserved REMOVE reservation_date";
+    
+    const expressionAttributeValues = reserved
+      ? { ":reserved": reserved, ":date": reservationDate }
+      : { ":reserved": reserved };
+
+    const command = new UpdateCommand({
+      TableName: this.tableName,
+      Key: { space_no: spaceNumber },
+      UpdateExpression: updateExpression,
+      ExpressionAttributeValues: expressionAttributeValues,
+      ReturnValues: "ALL_NEW",
+    });
+
+    logger.debug('Updating parking space reservation status', { 
+      spaceNumber, 
+      reserved, 
+      reservationDate 
+    });
+
+    const result = await databaseClient.executeCommand(command, 'update parking space');
+    return result.Attributes;
+  }
 }
 
-module.exports = getSpaceBySpaceNumber;
+module.exports = ParkingSpaceRepository;
