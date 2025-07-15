e97832e87344515160d6c4a6dd6c3f1a8f6efdb4
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-07-16 00:59:34 +0100

    Add/update file: utils/logger.js

---
+++ utils/logger.js
@@ -0,0 +1,55 @@
+const config = require('../config/environment');
+
+class Logger {
+  constructor() {
+    this.logLevel = config.app.logLevel;
+    this.levels = {
+      error: 0,
+      warn: 1,
+      info: 2,
+      debug: 3,
+    };
+  }
+
+  shouldLog(level) {
+    return this.levels[level] <= this.levels[this.logLevel];
+  }
+
+  formatMessage(level, message, meta = {}) {
+    const timestamp = new Date().toISOString();
+    const logEntry = {
+      timestamp,
+      level: level.toUpperCase(),
+      message,
+      ...meta,
+    };
+
+    return JSON.stringify(logEntry);
+  }
+
+  error(message, meta = {}) {
+    if (this.shouldLog('error')) {
+      console.error(this.formatMessage('error', message, meta));
+    }
+  }
+
+  warn(message, meta = {}) {
+    if (this.shouldLog('warn')) {
+      console.warn(this.formatMessage('warn', message, meta));
+    }
+  }
+
+  info(message, meta = {}) {
+    if (this.shouldLog('info')) {
+      console.info(this.formatMessage('info', message, meta));
+    }
+  }
+
+  debug(message, meta = {}) {
+    if (this.shouldLog('debug')) {
+      console.debug(this.formatMessage('debug', message, meta));
+    }
+  }
+}
+
+module.exports = new Logger();
