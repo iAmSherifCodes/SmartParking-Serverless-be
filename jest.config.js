d781843c31b59c7dec1e6ac2db091dcadfd9b054
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-07-16 00:59:31 +0100

    Add/update file: jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  collectCoverageFrom: [
+    'functions/**/*.js',
+    'services/**/*.js',
+    'repositories/**/*.js',
+    'utils/**/*.js',
+    '!**/node_modules/**',
+    '!coverage/**',
+    '!test/**'
+  ],
+  testMatch: [
+    '**/test/**/*.test.js',
+    '**/test/**/*.spec.js'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  testTimeout: 30000,
+  verbose: true
 }
